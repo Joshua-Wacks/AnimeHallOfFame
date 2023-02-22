5483669521cf61d995bab93ca809f26f187aa22d
Janco Venter <jan@bbd.co.za>
2023-02-22 22:47:47 +0200

    Build generator script and create script

---
+++ data_generator.sql
@@ -0,0 +1,441 @@
+-------------------- anime: Attack on Titan --------------------
+
+INSERT INTO anime_series
+VALUES ("Attack on Titan", "Set in a post-apocalyptic world where the remains of humanity live behind walls protecting them from giant humanoid Titans, Attack on Titan follows protagonist Eren Yeager, along with friends Mikasa Ackerman and Armin Arlert", 1)
+
+-------------------- season: 1 --------------------
+
+INSERT INTO anime_season
+VALUES (1, 1, 16, 24, 9, 2021)
+
+INSERT INTO anime_character
+VALUES (Eren Jaeger, 2045)
+
+INSERT INTO character_in_season
+VALUES (1, 1, 1)
+
+INSERT INTO hof_attributes
+VALUES (58, 36, 95, 18, 32, 18, 0)
+
+INSERT INTO anime_character
+VALUES (Mikasa Ackerman, 2061)
+
+INSERT INTO character_in_season
+VALUES (2, 1, 1)
+
+INSERT INTO hof_attributes
+VALUES (55, 35, 94, 15, 46, 76, 0)
+
+INSERT INTO anime_character
+VALUES (Erwin Smith, 1914)
+
+INSERT INTO character_in_season
+VALUES (3, 1, 1)
+
+INSERT INTO hof_attributes
+VALUES (86, 16, 36, 6, 81, 79, 0)
+
+INSERT INTO anime_character
+VALUES (Reiner Braun, 2038)
+
+INSERT INTO character_in_season
+VALUES (4, 1, 1)
+
+INSERT INTO hof_attributes
+VALUES (86, 53, 12, 90, 76, 2, 0)
+
+INSERT INTO anime_character
+VALUES (Armen Arlert, 2017)
+
+INSERT INTO character_in_season
+VALUES (5, 1, 1)
+
+INSERT INTO hof_attributes
+VALUES (72, 76, 23, 9, 61, 38, 0)
+
+-------------------- season: 2 --------------------
+
+INSERT INTO anime_season
+VALUES (1, 2, 12, 21, 4, 2012)
+
+INSERT INTO character_in_season
+VALUES (1, 1, 2)
+
+INSERT INTO hof_attributes
+VALUES (32, 12, 63, 28, 25, 97, 0)
+
+INSERT INTO character_in_season
+VALUES (2, 1, 2)
+
+INSERT INTO hof_attributes
+VALUES (31, 31, 48, 80, 66, 21, 0)
+
+INSERT INTO anime_character
+VALUES (Levi Ackerman, 2026)
+
+INSERT INTO character_in_season
+VALUES (6, 1, 2)
+
+INSERT INTO hof_attributes
+VALUES (1, 27, 21, 86, 98, 49, 0)
+
+INSERT INTO anime_character
+VALUES (Sasha Braus, 2051)
+
+INSERT INTO character_in_season
+VALUES (7, 1, 2)
+
+INSERT INTO hof_attributes
+VALUES (7, 61, 56, 10, 28, 55, 0)
+
+INSERT INTO character_in_season
+VALUES (5, 1, 2)
+
+INSERT INTO hof_attributes
+VALUES (64, 26, 47, 50, 11, 69, 0)
+
+-------------------- season: 3 --------------------
+
+INSERT INTO anime_season
+VALUES (1, 3, 14, 20, 10, 2004)
+
+INSERT INTO character_in_season
+VALUES (1, 1, 3)
+
+INSERT INTO hof_attributes
+VALUES (30, 69, 59, 54, 94, 100, 0)
+
+INSERT INTO character_in_season
+VALUES (2, 1, 3)
+
+INSERT INTO hof_attributes
+VALUES (79, 42, 21, 62, 46, 51, 0)
+
+INSERT INTO character_in_season
+VALUES (7, 1, 3)
+
+INSERT INTO hof_attributes
+VALUES (77, 66, 5, 46, 74, 35, 0)
+
+INSERT INTO anime_character
+VALUES (Connie Springer, 2088)
+
+INSERT INTO character_in_season
+VALUES (8, 1, 3)
+
+INSERT INTO hof_attributes
+VALUES (12, 90, 54, 10, 41, 99, 0)
+
+INSERT INTO character_in_season
+VALUES (5, 1, 3)
+
+INSERT INTO hof_attributes
+VALUES (2, 57, 93, 5, 4, 43, 0)
+
+-------------------- season: 4 --------------------
+
+INSERT INTO anime_season
+VALUES (1, 4, 19, 22, 0, 2011)
+
+INSERT INTO character_in_season
+VALUES (1, 1, 4)
+
+INSERT INTO hof_attributes
+VALUES (61, 100, 61, 71, 10, 31, 0)
+
+INSERT INTO character_in_season
+VALUES (2, 1, 4)
+
+INSERT INTO hof_attributes
+VALUES (80, 17, 25, 17, 53, 9, 0)
+
+INSERT INTO character_in_season
+VALUES (6, 1, 4)
+
+INSERT INTO hof_attributes
+VALUES (67, 30, 90, 9, 20, 71, 0)
+
+INSERT INTO character_in_season
+VALUES (4, 1, 4)
+
+INSERT INTO hof_attributes
+VALUES (39, 95, 98, 58, 81, 92, 0)
+
+INSERT INTO character_in_season
+VALUES (5, 1, 4)
+
+INSERT INTO hof_attributes
+VALUES (12, 99, 81, 55, 29, 6, 0)
+
+-------------------- anime: Naruto --------------------
+
+INSERT INTO anime_series
+VALUES ("Naruto", "On the day of Naruto Uzamaki's birth the village of Konoha was attacked by the 9-tailed fox demon. In order to protect the village Naruto's father the 4th hokage sacrificed his life and sealed the demon in his new born son. 13 years later Naruto graduates the ninja academy and becomes a shinobi with goal to be the hokage of the village. Joining him are rival Sasuke Uchiha who attempts to gain power to avenge his clan after they were murdered by his older brother Itachi. And Sakura Haruno who is Naruto's love interest who of course loves his rival Sasuke.", 0)
+
+-------------------- season: 1 --------------------
+
+INSERT INTO anime_season
+VALUES (2, 1, 12, 22, 4, 2017)
+
+INSERT INTO anime_character
+VALUES (Kakashi Hatake, 2089)
+
+INSERT INTO character_in_season
+VALUES (9, 2, 1)
+
+INSERT INTO hof_attributes
+VALUES (89, 45, 48, 11, 97, 38, 0)
+
+INSERT INTO anime_character
+VALUES (Naruto Uzumaki, 2092)
+
+INSERT INTO character_in_season
+VALUES (10, 2, 1)
+
+INSERT INTO hof_attributes
+VALUES (6, 65, 73, 57, 81, 52, 0)
+
+INSERT INTO anime_character
+VALUES (Might Guy, 1936)
+
+INSERT INTO character_in_season
+VALUES (11, 2, 1)
+
+INSERT INTO hof_attributes
+VALUES (34, 28, 50, 37, 41, 42, 0)
+
+INSERT INTO anime_character
+VALUES (Sasuke Uchiha, 2065)
+
+INSERT INTO character_in_season
+VALUES (12, 2, 1)
+
+INSERT INTO hof_attributes
+VALUES (83, 42, 53, 35, 82, 44, 0)
+
+INSERT INTO anime_character
+VALUES (Gaara, 1927)
+
+INSERT INTO character_in_season
+VALUES (13, 2, 1)
+
+INSERT INTO hof_attributes
+VALUES (19, 48, 29, 31, 52, 55, 0)
+
+-------------------- season: 2 --------------------
+
+INSERT INTO anime_season
+VALUES (2, 2, 16, 20, 6, 2021)
+
+INSERT INTO character_in_season
+VALUES (9, 2, 2)
+
+INSERT INTO hof_attributes
+VALUES (69, 99, 20, 87, 71, 81, 0)
+
+INSERT INTO character_in_season
+VALUES (10, 2, 2)
+
+INSERT INTO hof_attributes
+VALUES (67, 49, 76, 16, 28, 87, 0)
+
+INSERT INTO anime_character
+VALUES (Shikamaru Nara, 1941)
+
+INSERT INTO character_in_season
+VALUES (14, 2, 2)
+
+INSERT INTO hof_attributes
+VALUES (94, 88, 39, 4, 86, 79, 0)
+
+INSERT INTO anime_character
+VALUES (Rock Lee, 1925)
+
+INSERT INTO character_in_season
+VALUES (15, 2, 2)
+
+INSERT INTO hof_attributes
+VALUES (89, 34, 24, 88, 66, 1, 0)
+
+INSERT INTO character_in_season
+VALUES (13, 2, 2)
+
+INSERT INTO hof_attributes
+VALUES (77, 73, 55, 53, 60, 36, 0)
+
+-------------------- anime: Hunter x Hunter --------------------
+
+INSERT INTO anime_series
+VALUES ("Hunter x Hunter", "he story focuses on a young boy named Gon Freecss who discovers that his father, who left him at a young age, is actually a world-renowned Hunter, a licensed professional who specializes in fantastical pursuits such as locating rare or unidentified animal species, treasure hunting, surveying unexplored enclaves, or hunting down lawless individuals. Gon departs on a journey to become a Hunter and eventually find his father. Along the way, Gon meets various other Hunters and encounters the paranormal.", 0)
+
+-------------------- season: 1 --------------------
+
+INSERT INTO anime_season
+VALUES (3, 1, 24, 22, 1, 2017)
+
+INSERT INTO anime_character
+VALUES (Gon Freecss, 1901)
+
+INSERT INTO character_in_season
+VALUES (16, 3, 1)
+
+INSERT INTO hof_attributes
+VALUES (74, 84, 20, 61, 88, 76, 0)
+
+INSERT INTO anime_character
+VALUES (Killua Zoldyck, 2081)
+
+INSERT INTO character_in_season
+VALUES (17, 3, 1)
+
+INSERT INTO hof_attributes
+VALUES (49, 63, 67, 49, 70, 92, 0)
+
+INSERT INTO anime_character
+VALUES (Hisoka Morow, 2044)
+
+INSERT INTO character_in_season
+VALUES (18, 3, 1)
+
+INSERT INTO hof_attributes
+VALUES (85, 73, 11, 69, 67, 88, 0)
+
+INSERT INTO anime_character
+VALUES (Kurapika, 2085)
+
+INSERT INTO character_in_season
+VALUES (19, 3, 1)
+
+INSERT INTO hof_attributes
+VALUES (32, 30, 35, 41, 94, 20, 0)
+
+INSERT INTO anime_character
+VALUES (Leorio Paladiknight, 2091)
+
+INSERT INTO character_in_season
+VALUES (20, 3, 1)
+
+INSERT INTO hof_attributes
+VALUES (72, 82, 36, 18, 65, 75, 0)
+
+-------------------- anime: Jujutsu Kaisen --------------------
+
+INSERT INTO anime_series
+VALUES ("Jujutsu Kaisen", "Yuji Itadori, a kind-hearted teenager, joins his school's Occult Club for fun, but discovers that its members are actual sorcerers who can manipulate the energy between beings for their own use. He hears about a cursed talisman - the finger of Sukuna, a demon - and its being targeted by other cursed beings. Yuji eats the finger to protect his friends, and ends up becoming Sukuna's host. However, Yuji discovers that he has inherited magic and is able to control this power without interference from Sukuna. He joins the Tokyo Metropolitan Magic Technical College to consumes all of Sukuna's fingers, which will enable a full exorcism to take place that will free him.", 1)
+
+-------------------- season: 1 --------------------
+
+INSERT INTO anime_season
+VALUES (4, 1, 17, 23, 5, 2019)
+
+INSERT INTO anime_character
+VALUES (Yuji Itadori, 1962)
+
+INSERT INTO character_in_season
+VALUES (21, 4, 1)
+
+INSERT INTO hof_attributes
+VALUES (66, 20, 97, 60, 25, 13, 0)
+
+INSERT INTO anime_character
+VALUES (Megumi Fushiguro, 1906)
+
+INSERT INTO character_in_season
+VALUES (22, 4, 1)
+
+INSERT INTO hof_attributes
+VALUES (11, 18, 13, 85, 29, 50, 0)
+
+INSERT INTO anime_character
+VALUES (Nobara Kugisaki, 1980)
+
+INSERT INTO character_in_season
+VALUES (23, 4, 1)
+
+INSERT INTO hof_attributes
+VALUES (54, 72, 22, 14, 42, 41, 0)
+
+INSERT INTO anime_character
+VALUES (Satoru Gojo, 1913)
+
+INSERT INTO character_in_season
+VALUES (24, 4, 1)
+
+INSERT INTO hof_attributes
+VALUES (31, 40, 23, 90, 30, 45, 0)
+
+-------------------- anime: Demon Slayer --------------------
+
+INSERT INTO anime_series
+VALUES ("Demon Slayer", "A family is attacked by demons and only two members survive - Tanjiro and his sister Nezuko, who is turning into a demon slowly. Tanjiro sets out to become a demon slayer to avenge his family and cure his sister.", 1)
+
+-------------------- season: 1 --------------------
+
+INSERT INTO anime_season
+VALUES (5, 1, 14, 24, 5, 2006)
+
+INSERT INTO anime_character
+VALUES (Tanjiro Kamado, 2005)
+
+INSERT INTO character_in_season
+VALUES (25, 5, 1)
+
+INSERT INTO hof_attributes
+VALUES (63, 26, 47, 42, 57, 4, 0)
+
+INSERT INTO anime_character
+VALUES (Nezuko Kamado, 2009)
+
+INSERT INTO character_in_season
+VALUES (26, 5, 1)
+
+INSERT INTO hof_attributes
+VALUES (73, 95, 79, 84, 61, 99, 0)
+
+INSERT INTO anime_character
+VALUES (Zenitsu Agatsuma, 1936)
+
+INSERT INTO character_in_season
+VALUES (27, 5, 1)
+
+INSERT INTO hof_attributes
+VALUES (61, 32, 71, 98, 99, 9, 0)
+
+INSERT INTO anime_character
+VALUES (Inosuke Hashibira, 2059)
+
+INSERT INTO character_in_season
+VALUES (28, 5, 1)
+
+INSERT INTO hof_attributes
+VALUES (70, 23, 48, 42, 39, 60, 0)
+
+-------------------- season: 2 --------------------
+
+INSERT INTO anime_season
+VALUES (5, 2, 22, 21, 4, 2001)
+
+INSERT INTO character_in_season
+VALUES (25, 5, 2)
+
+INSERT INTO hof_attributes
+VALUES (25, 41, 47, 86, 50, 42, 0)
+
+INSERT INTO character_in_season
+VALUES (26, 5, 2)
+
+INSERT INTO hof_attributes
+VALUES (18, 91, 100, 53, 94, 7, 0)
+
+INSERT INTO character_in_season
+VALUES (27, 5, 2)
+
+INSERT INTO hof_attributes
+VALUES (16, 24, 76, 47, 78, 11, 0)
+
+INSERT INTO character_in_season
+VALUES (28, 5, 2)
+
+INSERT INTO hof_attributes
+VALUES (65, 36, 13, 94, 4, 95, 0)
+
