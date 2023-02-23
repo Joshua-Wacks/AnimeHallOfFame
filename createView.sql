3ec06c821e266f9128d1d5548d1b3af5a0b52713
Joaquim-Chambuara <122440707+Joaquim-Chambuara@users.noreply.github.com>
2023-02-23 10:40:32 +0200

    Create createView.sql

---
+++ createView.sql
@@ -0,0 +1,5 @@
+CREATE VIEW dbo.AnimeSeasonView
+AS
+SELECT anime_id, season_number, episodes, episode_duration, rating, year_released, episodes * episode_duration AS total_duration
+FROM dbo.AnimeSeason;
+GO
