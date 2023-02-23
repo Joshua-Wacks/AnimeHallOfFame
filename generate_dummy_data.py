7896f2c79129f833c69e523fa0c5d4578d38bd9e
Janco Venter <jan@bbd.co.za>
2023-02-23 13:59:06 +0200

    Change to single quotations

--- generate_dummy_data.py
+++ generate_dummy_data.py
@@ -57,7 +57,7 @@ for key in anime_info:
     anime = anime_info[key]
     sql += f"-------------------- anime: {anime['anime_series_name']} --------------------\n\n"
     sql += "INSERT INTO anime_series\n" \
-        f"VALUES (\"{anime['anime_series_name']}\", \"{anime['anime_series_description']}\", {anime['anime_series_ongoing']})\n\n"
+        f"VALUES ('{anime['anime_series_name']}', '{anime['anime_series_description']}', {anime['anime_series_ongoing']})\n\n"
     
     for season in anime["anime_seasons"]:
         sql += f"-------------------- season: {season} --------------------\n\n"
